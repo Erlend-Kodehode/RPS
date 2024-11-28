973c1735e5c1d6f6228336d6494195d4f330c195
Erlend <erlendk.kodehode@gmail.com>
2024-11-28 21:23:23 +0100

    base logic

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
+import gsap from "gsap";
+const rockbtn = document.querySelector("#rock");
+const paperbtn = document.querySelector("#paper");
+const scissorbtn = document.querySelector("#scissors");
+const btnContainer = document.querySelector("#btn-container");
 
+const buttons = Array.from(btnContainer.children);
+
+buttons.forEach((button, i) => {
+  button.addEventListener("click", () => {
+    playRPS(i);
+  });
+});
+
+function playRPS(playerMove) {
+  const randomMove = Math.floor(Math.random() * 3);
+  if (playerMove === randomMove) {
+    console.log("draw");
+  } else {
+    switch (randomMove) {
+      case 0:
+        console.log("rock");
+        if (playerMove === 1) {
+          console.log("win");
+        } else {
+          console.log("lose");
+        }
+        break;
+
+      case 1:
+        console.log("paper");
+        if (playerMove === 2) {
+          console.log("win");
+        } else {
+          console.log("lose");
+        }
+        break;
+
+      case 2:
+        console.log("scissors");
+        if (playerMove === 0) {
+          console.log("win");
+        } else {
+          console.log("lose");
+        }
+        break;
+    }
+  }
+}
