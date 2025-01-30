22715908eb3c4524eb736aa5a5793673fb33db55
Erlend <erlendk.kodehode@gmail.com>
2025-01-30 09:20:03 +0100

    import fix test

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import gsap from "./node_modules/gsap/all.js";
+import { gsap } from "gsap.js";
 const btnContainer = document.querySelector("#rps-container");
 const opponentCard = document.querySelector("#opponent");
 const playerCard = document.querySelector("#player");
